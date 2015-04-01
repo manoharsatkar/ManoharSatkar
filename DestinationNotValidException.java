2d28a0a0c96e5f0d2bd6565366a38fbd03c139a2
manoharsatkar <manohar.satkar@yahoo.com>
2015-04-01 23:04:17 +0530

    Create DestinationNotValidException.java

---
+++ DestinationNotValidException.java
@@ -0,0 +1,20 @@
+package com.denver;
+/**
+ * 
+ * @author manohar
+ *
+ */
+public class DestinationNotValidException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+	public DestinationNotValidException(String error){
+		super(error);
+	}
+	
+}
